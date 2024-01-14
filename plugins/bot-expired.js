d10e0c96c3d2246d52126887beecf0a51fbf8365
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-14 21:07:53 +0200

    Update bot-expired.js

--- plugins/bot-expired.js
+++ plugins/bot-expired.js
@@ -8,7 +8,7 @@ let chats = global.db.data.chats[m.chat]
 if (!chats.expired)
 return !0
 if (+new Date() > chats.expired) {
-await conn.reply(m.chat, 'El tiempo se a acabado!! el bot abandonará el grupo👋🏻')
+await conn.reply(m.chat, 'لقد انتهي الوقت ساأترك الجروب الان👋🏻')
 await conn.groupLeave(m.chat)
 chats.expired = null
 
