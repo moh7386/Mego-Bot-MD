ffe69bced5bc45a3b9127a9b801d0eedff683bb3
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-14 23:17:55 +0200

    Update config-antitrabas.js

--- plugins/config-antitrabas.js
+++ plugins/config-antitrabas.js
@@ -11,12 +11,12 @@ let bang = m.key.id
 let name = await conn.getName(m.sender)
 
 if (chat.antiTraba && m.text.length > 5000) { //Cantidad máxima de caracteres aceptados en un mensaje//
-if (isAdmin) return conn.sendMessage(m.chat, { text: `🚩 *El administrador @${m.sender.split("@")[0]} acaba de enviar un texto que contiene muchos caracteres*`, mentions: [m.sender] }, { quoted: fkontak })
-await conn.sendMessage(m.chat, `🚩 *Se detecto un mensaje que contiene muchos caracteres*\n`, `${isBotAdmin ? '' : 'No soy administrador, no puedo hacer nada :/'}`, m)
+if (isAdmin) return conn.sendMessage(m.chat, { text: `🚩 *مسؤول @${m.sender.split("@")[0]} لقد أرسلت للتو رسالة نصية تحتوي على العديد من الأحرف*`, mentions: [m.sender] }, { quoted: fkontak })
+await conn.sendMessage(m.chat, `🚩 *تم اكتشاف رسالة تحتوي على العديد من الأحرف*\n`, `${isBotAdmin ? '' : 'أنا لست مسؤولا، لا أستطيع أن أفعل أي شيء :/'}`, m)
 if (isBotAdmin && bot.restrict) {
 conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
 setTimeout(() => { 
-conn.sendMessage(m.chat, { text: `🎌 *Marcar el chat como leido*\n${"\n".repeat(400)}\n=> El número : wa.me/${m.sender.split("@")[0]}\n=> Alias : ${name}\nAcaba de enviar un texto que contiene muchos caracteres que puede ocasionar fallos en los dispositivos`, mentions: [m.sender] }, { quoted: fkontak })
+conn.sendMessage(m.chat, { text: `🎌 *وضع علامة على الدردشة كمقروءة*\n${"\n".repeat(400)}\n=> الرقم : wa.me/${m.sender.split("@")[0]}\n=> Alias : ${name}\nلقد قمت للتو بإرسال رسالة نصية تحتوي على العديد من الأحرف التي قد تتسبب في فشل الجهاز`, mentions: [m.sender] }, { quoted: fkontak })
 }, 0)
 setTimeout(() => { 
 conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
