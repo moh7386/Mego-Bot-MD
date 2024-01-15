db6b4a4af8558d61d778e714cca803feec066694
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-16 00:16:50 +0200

    Update descargas-mediafire.js

--- plugins/descargas-mediafire.js
+++ plugins/descargas-mediafire.js
@@ -7,8 +7,8 @@ let limit
 if((isOwner || isPrems)) limit = 1000
 else limit = 600
 
-if (!args[0]) return conn.reply(m.chat, `🎌 *Ingrese un enlace de mediafire*\n\nEjemplo, !mediafire https://www.mediafire.com/file/941xczxhn27qbby/GBWA_V12.25FF-By.SamMods-.apk`, m, fake, )
-if (!args[0].match(/mediafire/gi)) conn.reply(m.chat, `🚩 *Enlace incorrecto*`, m, fake, )
+if (!args[0]) return conn.reply(m.chat, `🎌 *أدخل رابط الميديا ​​فاير*\n\nمثال, ! ميديافاير https://www.mediafire.com/file/941xczxhn27qbby/GBWA_V12.25FF-By.SamMods-.apk`, m, fake, )
+if (!args[0].match(/mediafire/gi)) conn.reply(m.chat, `🚩 *رابط سيء*`, m, fake, )
 
 try {
 
@@ -20,18 +20,18 @@ let res = await mediafiredl(args[0])
 let { url, url2, filename, ext, aploud, filesize, filesizeH } = res
 let isLimit = (isPrems || isOwner ? limit : limit) * 1012 < filesize
 
-await conn.reply(m.chat, `💌 *Nombre:* ${filename}\n📊 *Peso:*  ${filesizeH}\n🗂️ *Tipo:* ${ext}\n🗳️ *Subido:* ${aploud}`, m, fake, )
+await conn.reply(m.chat, `💌 *اسم:* ${filename}\n📊 *وزن:*  ${filesizeH}\n🗂️ *يكتب:* ${ext}\n🗳️ *تم الرفع:* ${aploud}`, m, fake, )
     
 if(!isLimit) await conn.sendFile(m.chat, url, filename, '', m, null, { mimetype: ext, asDocument: true })
 m.react(done)
 } catch (e) {
-conn.reply(m.chat, `🚩 *Ocurrió un fallo*`, m, fake, )
+conn.reply(m.chat, `🚩 *حدث فشل*`, m, fake, )
 console.log(e)}
 
 }
 handler.help = ['mediafire']
 handler.tags = ['descargas']
-handler.command = ['mediafire', 'mfire']
+handler.command = ['mediafire', 'ميديافاير']
 
 handler.diamond = true
 handler.register = true
