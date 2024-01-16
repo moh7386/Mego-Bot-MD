e2a33c401d0ab1855a8a59c8c85dc48cc3eb9dde
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-17 00:52:39 +0200

    Update grupo-setbye.js

--- plugins/grupo-setbye.js
+++ plugins/grupo-setbye.js
@@ -4,13 +4,13 @@ if (text) {
 if (isROwner) global.conn.bye = text
 else if (isOwner) conn.bye = text
 else global.db.data.chats.sBye = text
-conn.reply(m.chat, '✅ *Configurado correctamente*', m, fake, )
-} else return conn.reply(m.chat, '🎌 *Ingrese el texto que quiere que se envíe al salirse una persona*\n\nPuede usar @user para etiquetar', m, fake, )
+conn.reply(m.chat, '✅ *تم بشكل صحيح*', m, fake, )
+} else return conn.reply(m.chat, '🎌 *أدخل النص الذي تريد إرساله عندما يغادر الشخص*\n\يمكنك استخدام @user لوضع علامة', m, fake, )
 
 }
 handler.help = ['setbye']
 handler.tags = ['grupo']
-handler.command = /^setbye$/i
+handler.command = /^تغير_المغادره$/i
 
 handler.group = true
 handler.admin = true
