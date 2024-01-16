cd0401bba67c8cd1f653cdd7989bdcc81e4ce575
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-16 23:47:51 +0200

    Update grupo-kick.js

--- plugins/grupo-kick.js
+++ plugins/grupo-kick.js
@@ -1,6 +1,6 @@
 var handler = async (m, { conn, participants, usedPrefix, command }) => {
 
-if (!m.mentionedJid[0] && !m.quoted) return conn.reply(m.chat, '🎌 *Etiquete o responda al mensaje de la persona que quiere eliminar*', m, fake, ) 
+if (!m.mentionedJid[0] && !m.quoted) return conn.reply(m.chat, '🎌 *ضع علامة على رسالة الشخص الذي تريد إزالته أو قم بالرد عليها*', m, fake, ) 
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 
 await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
@@ -8,7 +8,7 @@ await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
 }
 handler.help = ['kick']
 handler.tags = ['grupo']
-handler.command = /^(kick|echar|hechar|sacar|ban)$/i
+handler.command = /^(kick|طرد|هش|sacar|براا)$/i
 
 handler.admin = true
 handler.group = true
