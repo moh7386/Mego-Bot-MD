0ff605945f98e29ab105ddbe6ccc43f1194c0aed
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-17 01:46:54 +0200

    Update grupo-setdesc.js

--- plugins/grupo-setdesc.js
+++ plugins/grupo-setdesc.js
@@ -2,13 +2,13 @@ var handler = async (m, { conn, text, isROwner, isOwner }) => {
 
 if (text) {
 await conn.groupUpdateDescription(m.chat, text)
-conn.reply(m.chat, `✅ *Se actualizo la descripción con éxito*`, m, fake, )
-} else return conn.reply(m.chat, '🎌 *Ingrese la nueva descripción para el grupo*\n\nEjmplo, !setdesc Welcome to the group', m, fake, )
+conn.reply(m.chat, `✅ *تم التعديل*`, m, fake, )
+} else return conn.reply(m.chat, '🎌 *ادخل الوصف الجديد*\n\nمثال, .تغيير_الوصف مرحبا بكم في الجروب', m, fake, )
 
 }
 handler.help = ['setdesc']
 handler.tags = ['grupo']
-handler.command = /^set(desk)?(desc)$/i
+handler.command = /^set(desk)?(desc)|تغيير_الوصف$/i
 
 handler.botAdmin = true
 handler.group = true
