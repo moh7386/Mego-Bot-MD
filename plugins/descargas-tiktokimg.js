0d0d2dcee8404bb79514bd42831f7d4c6264be1d
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-16 09:12:03 +0200

    Update descargas-tiktokimg.js

--- plugins/descargas-tiktokimg.js
+++ plugins/descargas-tiktokimg.js
@@ -2,11 +2,11 @@ import fetch from 'node-fetch'
 
 var handler = async(m, { conn, text, usedPrefix, command }) => {
 
-if (!text) return conn.reply(m.chat, `🎌 *Ingrese un enlace de tiktok que contenga imágenes*`, m, fake, )
+if (!text) return conn.reply(m.chat, `🎌 *ادخل رابط تيك توك الصور*`, m, fake, )
 m.react(rwait)
-if (!(text.includes('http://') || text.includes('https://'))) return conn.reply(m.chat, `🚩 *Este enlace no contiene http:// ni https://*`, m, fake, )
+if (!(text.includes('http://') || text.includes('https://'))) return conn.reply(m.chat, `🚩 *لا يحتوي هذا الرابط http:// ni https://*`, m, fake, )
 m.react(error)
-if (!text.includes('tiktok.com')) return conn.reply(m.chat, `🚩 *Url errónea*`, m, fake, )
+if (!text.includes('tiktok.com')) return conn.reply(m.chat, `🚩 *رابط خطأ*`, m, fake, )
 m.react(error)
 
 try {
@@ -18,7 +18,7 @@ anu = anu.result
 if (anu.length == 0) throw Error('Error : no data')
 let c = 0
 for (let x of anu) {
-if (c == 0) await conn.sendMessage(m.chat, { image: { url: x }, caption: `✅ *Se ha enviado 1 de ${anu.length} imágenes.*\n_El resto podrá ser visible en el chat privado del bot_` }, { quoted : m })
+if (c == 0) await conn.sendMessage(m.chat, { image: { url: x }, caption: `✅ *تم إرسال 1 منها ${anu.length} الصور.*\nقد يكون الباقي مرئيًا في الدردشة الخاصة بالروبوت` }, { quoted : m })
 else await conn.sendMessage(m.sender, { image: { url: x } }, { quoted : m })
 c += 1
 }
@@ -26,12 +26,12 @@ m.react(done)
 } catch (e) {
 console.log(e)
 m.react(error)
-return conn.reply(m.chat, `🚩 *Ocurrió un fallo*`, m, fake, )
+return conn.reply(m.chat, `🚩 *حدث فشل*`, m, fake, )
 }
 
 }
 handler.help = ['tiktokslide']
 handler.tags = ['descargador']
-handler.command = /^((tt|tiktok)imagen)$/i
+handler.command = /^(تيك_صور)$/i
 
 export default handler
