67421ccabdac8305593537e962d07f342034f148
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-16 10:25:56 +0200

    Update descargas-yta.js

--- plugins/descargas-yta.js
+++ plugins/descargas-yta.js
@@ -63,7 +63,7 @@ await conn.reply(m.chat, '🚩 *حدث فشل*', m, fake, )}
 }
 handler.help = ['yta']
 handler.tags = ['descargas']
-handler.command = /^اغنيه2|fgmp3|dlmp3|getaud|yt(a|mp3)$/i
+handler.command = /^اغاني|fgmp3|dlmp3|getaud|yt(a|mp3)$/i
 
 handler.limit = true
 
