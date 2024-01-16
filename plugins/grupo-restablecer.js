86885f24774ba8f97e52f8d87df890244627af10
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-17 00:42:37 +0200

    Update grupo-restablecer.js

--- plugins/grupo-restablecer.js
+++ plugins/grupo-restablecer.js
@@ -1,16 +1,11 @@
-var handler = async (m, { conn }) => {
+/* Creditos a https://github.com/ALBERTO9883 */
 
-let res = await conn.groupRevokeInvite(m.chat)
-let gruf = m.chat
-conn.reply(m.sender, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(gruf), m, fake, )
-
-}
-handler.help = ['revoke']
-handler.tags = ['grupo']
-handler.command = /^re(voke|new)(invite|link|restablecerlink)?$/i
-
-handler.group = true
-handler.admin = true
-handler.botAdmin = true
-
-export default handler
+const handler = async (m, {conn}) => {
+  const revoke = await conn.groupRevokeInvite(m.chat);
+  await conn.reply(m.chat, `🔹️ *_تم تغير لينك الجروب._*\n♾ • اللينك الجديد: ${'https://chat.whatsapp.com/' + revoke}`, m);
+};
+handler.command = ['رستر', 'تغيير_اللينك'];
+handler.botAdmin = true;
+handler.admin = true;
+handler.group = true;
+export default handler;
