4fa37059e95253d8424fe4d145645401060235c7
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-17 01:55:42 +0200

    Update grupo-setname.js

--- plugins/grupo-setname.js
+++ plugins/grupo-setname.js
@@ -1,13 +1,13 @@
 var handler = async (m, { conn, text, isROwner, isOwner }) => {
 
-if (!text) return conn.reply(m.chat, '🎌 *Ingrese el nuevo nombre del grupo*\n\nEjmplo, !setname Curiosity', m, fake, )
+if (!text) return conn.reply(m.chat, '🎌 *أدخل اسم المجموعة الجديد*\n\nمثال, تغيير_الاسم MEGO-BOT', m, fake, )
 await conn.groupUpdateSubject(m.chat, text)
-conn.reply(m.chat, `🚩 ${text ? `${text} *es el nuevo nombre de este grupo*` : '*No pusieron un nombre*'}`, m, fake, )
+conn.reply(m.chat, `🚩 ${text ? `${text} *اسم الجروب الجديد*` : '*لم يعطوا اسما*'}`, m, fake, )
  
 }
 handler.help = ['setname']
 handler.tags = ['grupo']
-handler.command = /^setname$/i
+handler.command = /^تغيير_الاسم$/i
 
 handler.botAdmin = true
 handler.group = true
