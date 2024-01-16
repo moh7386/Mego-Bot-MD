f04a3797bee2856d3e1fa9295518f21d623c346c
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-17 01:01:19 +0200

    Update grupo-add.js

--- plugins/grupo-add.js
+++ plugins/grupo-add.js
@@ -1,28 +1,37 @@
-var handler = async (m, { conn, args, text, usedPrefix, command }) => {
-
-let who 
-if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
-else who = m.chat
-let name = await conn.getName(m.sender)	
-let user = global.db.data.users[who]
-let nom = conn.getName(m.sender)
-if (!global.db.data.settings[conn.user.jid].restrict) return conn.reply(m.chat, `🚩 *تم تعطيل هذه الميزة*`, m, fake, )
-if (!text) return conn.reply(m.chat, `🎌 *أدخل رقم الشخص الذي تريد إضافته*\n\nمثال, !${command} 201012531172`, m, fake, )
-if (text.includes('+')) return conn.reply(m.chat, `🚩 *أدخل الرقم بالكامل معًا بدون (+)*`, m, fake, )
-let group = m.chat
-let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
- 
-await conn.reply(text+'@s.whatsapp.net', `🎌 *مرحبا لقد تم دعاءك اللي جروبنا*\n\nالرابط\n${link}`, m, {mentions: [m.sender]} )
-conn.reply(m.chat, `🎌 *تم ارسال الدعوة على الخاص*\n${nom}`, m, fake, ) 
-
-}
-handler.help = ['add']
-handler.tags = ['grupo']
-handler.command = /^(اضافة|اضافه|invitar|دعوه|añadir|\+)$/i
-
-handler.group = true
-handler.admin = true
-handler.botAdmin = true
-handler.fail = null
-
+let { generateWAMessageFromContent, prepareWAMessageMedia, proto } = (await import('@adiwajshing/baileys')).default
+import fetch from 'node-fetch'
+const { getBinaryNodeChild, getBinaryNodeChildren } = (await import('@adiwajshing/baileys')).default
+let handler = async (m, { conn, text, participants, args }) => {  
+if (!global.db.data.settings[conn.user.jid].restrict) throw '*『❍↜╎لقد تم تعطيل هاذا الامر بواسطة المالك』*'
+if (!args[0]) throw '*『❍↜╎ضع الرقم الذي تريد اضافته』*'    
+try {    
+let _participants = participants.map(user => user.id)
+let users = (await Promise.all(
+text.split(',')
+.map(v => v.replace(/[^0-9]/g, ''))
+.filter(v => v.length > 4 && v.length < 20 && !_participants.includes(v + '@s.whatsapp.net'))
+.map(async v => [v, await conn.onWhatsApp(v + '@s.whatsapp.net')]))).filter(v => v[1][0]?.exists).map(v => v[0] + '@c.us')
+const response = await conn.query({ tag: 'iq', attrs: { type: 'set', xmlns: 'w:g2', to: m.chat }, content: users.map(jid => ({ tag: 'add', attrs: {}, content: [{ tag: 'participant', attrs: { jid } }]}))})
+const pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
+const jpegThumbnail = pp ? await (await fetch(pp)).buffer() : Buffer.alloc(0)
+const add = getBinaryNodeChild(response, 'add')
+const participant = getBinaryNodeChildren(add, 'participant')
+for (const user of participant.filter(item => item.attrs.error == 403)) {
+const jid = user.attrs.jid
+const content = getBinaryNodeChild(user, 'add_request')
+const invite_code = content.attrs.code
+const invite_code_exp = content.attrs.expiration
+let teks = `*˼❄˹تم ارسال دعوة اليه بسبب انه مفعل  اعدادات الخصوصية@${jid.split('@')[0]},*`
+m.reply(teks, null, { mentions: conn.parseMention(teks)})
+let captionn = `مرحباا ، كيف حالك انا ميجو بوت تم ارسال لك دعوة لي دخول الجروب نحن ننتظر عودتك😁`    
+var messaa = await prepareWAMessageMedia({ image: jpegThumbnail }, { upload: conn.waUploadToServer })
+var groupInvite = generateWAMessageFromContent(m.chat, proto.Message.fromObject({ groupInviteMessage: { groupJid: m.chat, inviteCode: invite_code, inviteExpiration: invite_code_exp, groupName: await conn.getName(m.chat), caption: captionn, jpegThumbnail: jpegThumbnail }}), { userJid: jid })
+await conn.relayMessage(jid, groupInvite.message, { messageId: groupInvite.key.id })}
+} catch {
+throw '*˹♦❄قد يكون هاذا الرقم غير صحيح تاكد من الرقم و هحاول تعيد الامر*'
+}}
+handler.help = ['add', '+'].map(v => v + ' número')
+handler.tags = ['group']
+handler.command = /^(add|ضيف|اضافه|\+)$/i
+handler.admin = handler.group = handler.botAdmin = true
 export default handler
