6da4e8a1fbf37ea1aa1a38a58d61576ad0d32d1e
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-16 23:49:06 +0200

    Update grupo-link.js

--- plugins/grupo-link.js
+++ plugins/grupo-link.js
@@ -2,12 +2,12 @@ var handler = async (m, { conn, args }) => {
 
 let group = m.chat
 let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
-conn.reply(m.chat, '🚩\v' + link, fkontak, m, { detectLink: true })
+conn.reply(m.chat, '🚩\v' + الرابط, اتصال, m, { detectLink: true })
 
 }
 handler.help = ['link']
 handler.tags = ['grupo']
-handler.command = /^link(gro?up)?$/i
+handler.command = /^لينك(gro?up)?$/i
 
 handler.group = true
 handler.botAdmin = true
