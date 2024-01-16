dbbbdaa707b6c846852fa737aa6da4df48318d2d
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-16 20:28:22 +0200

    Update grupo-add.js

--- plugins/grupo-add.js
+++ plugins/grupo-add.js
@@ -6,19 +6,19 @@ else who = m.chat
 let name = await conn.getName(m.sender)	
 let user = global.db.data.users[who]
 let nom = conn.getName(m.sender)
-if (!global.db.data.settings[conn.user.jid].restrict) return conn.reply(m.chat, `🚩 *Esta característica esta deshabilitada*`, m, fake, )
-if (!text) return conn.reply(m.chat, `🎌 *Ingrese el número de la persona que quiere añadir*\n\nEjemplo, !${command} 5217299999999`, m, fake, )
-if (text.includes('+')) return conn.reply(m.chat, `🚩 *Ingrese todo el número junto sin el (+)*`, m, fake, )
+if (!global.db.data.settings[conn.user.jid].restrict) return conn.reply(m.chat, `🚩 *تم تعطيل هذه الميزة*`, m, fake, )
+if (!text) return conn.reply(m.chat, `🎌 *أدخل رقم الشخص الذي تريد إضافته*\n\nمثال, !${command} 201012531172`, m, fake, )
+if (text.includes('+')) return conn.reply(m.chat, `🚩 *أدخل الرقم بالكامل معًا بدون (+)*`, m, fake, )
 let group = m.chat
 let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
  
-await conn.reply(text+'@s.whatsapp.net', `🎌 *Hola! soy CuriosityBot-MD, Una persona te a invitado a su grupo*\n\nEnlace\n${link}`, m, {mentions: [m.sender]} )
-conn.reply(m.chat, `🎌 *La invitación fue enviada al privado de*\n${nom}`, m, fake, ) 
+await conn.reply(text+'@s.whatsapp.net', `🎌 *مرحبا لقد تم دعاءك اللي جروبنا*\n\nالرابط\n${link}`, m, {mentions: [m.sender]} )
+conn.reply(m.chat, `🎌 *تم ارسال الدعوة على الخاص*\n${nom}`, m, fake, ) 
 
 }
 handler.help = ['add']
 handler.tags = ['grupo']
-handler.command = /^(add|agregar|invitar|invite|añadir|\+)$/i
+handler.command = /^(اضافة|agregar|invitar|دعوه|añadir|\+)$/i
 
 handler.group = true
 handler.admin = true
