1e5be73de99372235da8ef05c5637a090457b8d4
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-17 22:54:45 +0200

    Update owner-backup.js

--- plugins/owner-backup.js
+++ plugins/owner-backup.js
@@ -2,18 +2,18 @@ import fs from 'fs'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-await m.reply(`_*🗂️ Enviando base de datos. . .*_`)
+await m.reply(`_*🗂️ إرسال قاعدة البيانات. . .*_`)
 try {
 let d = new Date
-let date = d.toLocaleDateString('es', { day: 'numeric', month: 'long', year: 'numeric' })
+let date = d.toLocaleDateString('ar', { day: 'numeric', month: 'long', year: 'numeric' })
 let database = await fs.readFileSync(`./database.json`)
-let creds = await fs.readFileSync(`./sessions/creds.json`)
-await conn.reply(m.sender, `*🗓️ Database:* ${date}`, fkontak)
+let creds = await fs.readFileSync(`./mego md/creds.json`)
+await conn.reply(m.sender, `*🗓️ قاعده البايانات:* ${date}`, fkontak)
 await conn.sendMessage(m.sender, {document: database, mimetype: 'application/json', fileName: `database.json`}, { quoted: m })
 await conn.sendMessage(m.sender, {document: creds, mimetype: 'application/json', fileName: `creds.json`}, { quoted: m })
 } catch (e) {
-await m.reply('FALLO')
-console.log(`FALLO`)
+await m.reply('فشل')
+console.log(`فشل`)
 console.log(e)}}
 
 handler.command = /^(backup|respaldo|copia)$/i
