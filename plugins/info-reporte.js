a08cf3d22c51f56b0d32f09cd99d4d053754a87e
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-17 19:32:04 +0200

    Update info-reporte.js

--- plugins/info-reporte.js
+++ plugins/info-reporte.js
@@ -4,7 +4,6 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
   if (text.length > 1000) throw `*[❗معلومه❗] لا ترسل اكثر من 1000 حرف!*`;
   const teks = `*❒═════[ابلاغ]═════❒*\n*┬*\n*├❧ الرقم:* wa.me/${m.sender.split`@`[0]}\n*┴*\n*┬*\n*├❧ الرساله:* ${text}\n*┴*`;
   conn.reply('201012531172@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
-  conn.reply('201012531172@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
   m.reply(`*[ ✔️ ]  تم بنجاح، تم إرسال التقرير إلى مطوري.  سيكون لديك الجواب قريبا.  إذا تم اكتشاف أن التقرير خاطئ، فسيتم تجاهل التقرير.*`);
 };
 handler.help = ['reporte', 'request'].map((v) => v + ' <teks>');
