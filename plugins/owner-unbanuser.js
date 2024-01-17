fa7164379cb8b2af4695f140c0f7bf1e8ea9154e
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-18 01:45:53 +0200

    Update owner-unbanuser.js

--- plugins/owner-unbanuser.js
+++ plugins/owner-unbanuser.js
@@ -1,14 +1,14 @@
 let handler = async (m, {conn, text}) => {
-  if (!text) throw "*⚠️ INGRESA EL @tag DE ALGUN USUARIO*"
+  if (!text) throw "*🚨 اعمل منشن للمستخدم*"
   let who
   if (m.isGroup) who = m.mentionedJid[0]
   else who = m.chat
-  if (!who) throw "*⚠️ INGRESA EL @tag DE ALGUN USUARIO*"
+  if (!who) throw "*🚨 اعمل منشن للمستخدم*"
   let users = global.db.data.users
   users[who].banned = false
-  conn.reply(m.chat, `*⏤͟͟͞͞🍓ESTE USUARIO FUE DESBANEADO CON ÉXITO*\n*[🌟] YA PUEDE USAR CURIOSITY BOT-MD*`, m)}
+  conn.reply(m.chat, `*⏤͟͟͞͞🚨 تم الغاء حظر هذا المستخدم بنجاح*`, m)}
 handler.help = ["unbanuser"]
 handler.tags = ["owner"]
-handler.command = /^unbanuser$/i
+handler.command = /^بان_فك$/i
 handler.rowner = true
 export default handler
