632cb438b3bd4fb38a8acee31fb82c4725e423e7
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-17 09:42:35 +0200

    Update implementos-qrcode.js

--- plugins/implementos-qrcode.js
+++ plugins/implementos-qrcode.js
@@ -2,12 +2,12 @@ import { toDataURL } from 'qrcode'
 
 var handler = async (m, { text, conn }) => {
 
-if (!text) throw `*⚠️ INGRESE EL TEXTO QUE QUIERE CONVERTIR EN CÓDIGO QR*`
-conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', '*🧃 CÓDIGO QR*', m)
+if (!text) throw `*⚠️ أدخل النص الذي تريد تحويله إلى رمز الاستجابة السريعة*`
+conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', '*🧃 تفضلQR*', m)
 
 }
 handler.help = ['qrcode']
 handler.tags = ['implementos']
-handler.command = /^qrcode$/i
+handler.command = /^باركود$/i
 
 export default handler
