352bf6bc6badefe1d507661d3f9a3aed222953da
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-17 02:06:11 +0200

    Update grupo-setwelcome.js

--- plugins/grupo-setwelcome.js
+++ plugins/grupo-setwelcome.js
@@ -5,12 +5,12 @@ if (isROwner) global.conn.welcome = text
 else if (isOwner) conn.welcome = text
 else global.db.data.chats.sWelcome = text
 
-conn.reply(m.chat, '✅ *La bienvenida se a configurado*', m, fake, )
-} else return conn.reply(m.chat, '🎌 *Ingrese el texto que quiere que envie al unirse una persona*\n\nPuede usar @user para etiquetar', m, fake, )
+conn.reply(m.chat, '✅ *تم تغيير الترحيب*', m, fake, )
+} else return conn.reply(m.chat, '🎌 *أدخل النص الذي تريد إرساله عند انضمام شخص ما*\n\nيمكنك استخدام @user لوضع علامة', m, fake, )
 }
 handler.help = ['setwelcome']
 handler.tags = ['grupo']
-handler.command = /^setwelcome$/i
+handler.command = /^تغيير_الدخول$/i
 
 handler.group = true
 handler.admin = true
