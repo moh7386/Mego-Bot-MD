9d0b34f8b6809ed3b3834904df04fb8eba1defae
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-18 00:24:20 +0200

    Update owner-ds.js

--- plugins/owner-ds.js
+++ plugins/owner-ds.js
@@ -6,17 +6,17 @@ import path from 'path'
 var handler = async (m, { conn, usedPrefix }) => {
 
 if (global.conn.user.jid !== conn.user.jid) {
-return conn.reply(m.chat, '🚩 *Utiliza este comando directamente en el número principal del Bot*', m, fake, )
+return conn.reply(m.chat, '🚩 *استخدم هذا الأمر مباشرة في الرقم الرئيسي للبوت*', m, fake, )
 }
-await conn.reply(m.chat, '🚩 *Iniciando proceso de eliminación de todos los archivos de sesión, excepto el archivo creds.json...*', m, fake, )
+await conn.reply(m.chat, '🚯 *بدء عملية حذف جميع ملفات الجلسة باستثناء الملفcreds.json...*', m, fake, )
 m.react(rwait)
 
-let sessionPath = './sessions/'
+let sessionPath = './mego md/'
 
 try {
 
 if (!existsSync(sessionPath)) {
-return await conn.reply(m.chat, '🚩 *La carpeta está vacía*', m, fake, )
+return await conn.reply(m.chat, '🚩 *المجلد فارغ*', m, fake, )
 }
 let files = await fs.readdir(sessionPath)
 let filesDeleted = 0
@@ -27,22 +27,22 @@ filesDeleted++;
 }
 }
 if (filesDeleted === 0) {
-await conn.reply(m.chat, '🚩 *La carpeta esta vacía*',  m, fake, )
+await conn.reply(m.chat, '🚩 *المجلد فارغ*',  m, fake, )
 } else {
 m.react(done)
-await conn.reply(m.chat, `🎌 *Se eliminaron ${filesDeleted} archivos de sesión, excepto el archivo creds.json*`,  m, fake, )
-conn.reply(m.chat, `😸 *¡Hola! ¿logras verme?*`, m, fake, )
+await conn.reply(m.chat, `🎌 *تم الحذف بنجاح ${filesDeleted}   باستثناء ملف creds.json*`,  m, fake, )
+conn.reply(m.chat, `👾 *¡مرحبا! ¿هل تراني يخوي?*`, m, fake, )
 
 }
 } catch (err) {
 console.error('Error al leer la carpeta o los archivos de sesión:', err);
-await conn.reply(m.chat, '🚩 *Ocurrió un fallo*',  m, fake, )
+await conn.reply(m.chat, '🚩 *حدث فشل*',  m, fake, )
 }
 
 }
 handler.help = ['dsowner']
 handler.tags = ['owner']
-handler.command = /^(del_reg_in_session_owner|dsowner|clearallsession)$/i
+handler.command = /^(del_reg_in_session_owner|تنظيف|clearallsession)$/i
 
 handler.rowner = true
 
