8a43dbe351aab1888fa55ff1bfaf387105d7917a
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-18 01:49:35 +0200

    Update propietario(a)-add_vip.js

--- plugins/propietario(a)-add_vip.js
+++ plugins/propietario(a)-add_vip.js
@@ -2,16 +2,16 @@ let handler = async (m, { conn, text, command, usedPrefix }) => {
 let who
 if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
 else who = m.chat
-if (!who) throw `*⚠️ ETIQUETE A LA PERSONA QUE VA A SER USUARIO VIP\n\n💡 EJEMPLO\n*${usedPrefix + command} @tag*`
-if (global.prems.includes(who.split`@`[0])) throw `*EL USUARIO YA ES VIP ✨*`
+if (!who) throw `*⚠️ ضع ريبلاي على الشخص الذي سيصبح مستخدمًا VIP\n\n💡 مثال\n*${usedPrefix + command} @tag*`
+if (global.prems.includes(who.split`@`[0])) throw `*المستخدم بالفعل VIP ✨*`
 global.prems.push(`${who.split`@`[0]}`)
-conn.reply(m.chat, `*@${who.split`@`[0]} AHORA ES USUARIO VIP. NO VA A TENER LÍMITES CON ${cb} 😏*`, m, {
+conn.reply(m.chat, `*@${who.split`@`[0]} أنت الآن مستخدم VIP.  لن يكون لديك حدود مع ${cb} 😏*`, m, {
 contextInfo: {
 mentionedJid: [who]
 }})}
 handler.help = ['addprem <@user>']
 handler.tags = ['owner']
-handler.command = /^(add|\+)prem$/i
+handler.command = /^(add|\+)prem|بريم$/i
 handler.group = true
 handler.admin = true
 handler.botAdmin = true
