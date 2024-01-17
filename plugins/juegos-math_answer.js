5201a61450adb1a60e119ed547687d393c80a75e
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-17 21:04:58 +0200

    Update juegos-math_answer.js

--- plugins/juegos-math_answer.js
+++ plugins/juegos-math_answer.js
@@ -3,22 +3,22 @@ handler.before = async function (m) {
 
 if (!/^-?[0-9]+(\.[0-9]+)?$/.test(m.text)) return !0
 let id = m.chat
-if (!m.quoted || !m.quoted.fromMe || !m.text || !/^🧮 Cuanto es/i.test(m.quoted.text)) return !0
+if (!m.quoted || !m.quoted.fromMe || !m.text || !/^🧮 كم/i.test(m.quoted.text)) return !0
 this.math = this.math ? this.math : {}
-if (!(id in this.math)) return this.reply(m.chat, '⚠️ *EL JUEGO A TERMINADO*', m)
+if (!(id in this.math)) return this.reply(m.chat, '⚠️ *انتهت اللعبة*', m)
 if (m.quoted.id == this.math[id][0].id) {
 let math = JSON.parse(JSON.stringify(this.math[id][1]))
 if (m.text == math.result) {
 global.db.data.users[m.sender].exp += math.bonus
 clearTimeout(this.math[id][3])
 delete this.math[id]
-m.reply(`✅ *RESPUESTA CORRECTA!*\n\n‣ Ganaste : *+${math.bonus} XP*`)
+m.reply(`✅ *اجابة صحيحة!*\n\n‣ فاز : *+${math.bonus} XP*`)
 } else {
 if (--this.math[id][2] == 0) {
 clearTimeout(this.math[id][3])
 delete this.math[id]
-m.reply(`*❌ SE TERMINARON LAS OPORTUNIDADES*\n\n Respuesta : *${math.result}*`)
-} else m.reply(`⚠️ *RESPUESTA INCORRECTA*\n\nTodavia hay  ${this.math[id][2]} oportunidades`)
+m.reply(`*❌ لقد انتهت الفرص*\n\n اجابه : *${math.result}*`)
+} else m.reply(`⚠️ *إجابة خاطئة*\n\nلا تزال هناك  ${this.math[id][2]} فرص`)
 }}
 return !0
 }
