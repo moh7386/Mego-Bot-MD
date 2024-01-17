df78809b752ea95e03ca5be839fb5309db72c502
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-18 01:20:18 +0200

    Update owner-leavegc.js

--- plugins/owner-leavegc.js
+++ plugins/owner-leavegc.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn, text, command }) => {
 let id = text ? text : m.chat  
-await conn.reply(id, '*Adios a todos, el Bot se despide! (≧ω≦)ゞ*') 
+await conn.reply(id, '*وداعا احبكم سأخرج الان من الجروب! (≧ω≦)ゞ*') 
 await conn.groupLeave(id)}
 handler.tags = ['owner']
-handler.command = /^(out|leavegc|leave|salirdelgrupo)$/i
+handler.command = /^(اخرج|leavegc|برا|salirdelgrupo)$/i
 handler.group = true
 handler.rowner = true
 export default handler
