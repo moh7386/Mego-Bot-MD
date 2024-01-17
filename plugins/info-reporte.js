b4ad9344c17da4aad5021cb1c425745136228add
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-17 19:06:44 +0200

    Update info-reporte.js

--- plugins/info-reporte.js
+++ plugins/info-reporte.js
@@ -1,18 +1,18 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) throw `*[⚠️] ESCRIBE EL REPORTE*\n\n💡 EJEMPLO\n*${usedPrefix + command} el comando ${usedPrefix}infobot no funciona.*`
-if (text.length < 8) throw `⚠️ *Mínimo 10 caracteres para hacer El Reporte.*`
-if (text.length > 1000) throw `⚠️ *Máximo 1000 caracteres para hacer El Reporte.*`
-let teks = `*⚠️ REPORTE ⚠️*\n*📞 NÚMERO*\nWa.me/${m.sender.split`@`[0]}\n*📝 MENSAJE*\n${text}`
+if (!text) throw `*[⚠️] اكتب التقرير*\n\n💡 مثال\n*${usedPrefix + command} الامر ${usedPrefix}البوت به خطأ.*`
+if (text.length < 8) throw `⚠️ *الحد الأدنى 10 أحرف.*`
+if (text.length > 1000) throw `⚠️ *الحد الأقصى 1000 حرف.*`
+let teks = `*⚠️ خطأ ⚠️*\n*📞 الرقم*\nWa.me/${m.sender.split`@`[0]}\n*📝 رساله*\n${text}`
 conn.reply('120363145586610812@g.us', m.quoted ? teks + m.quoted.text : teks, null, {
 contextInfo: {
 mentionedJid: [m.sender]
 }})
-  m.reply(`*[⚠️] Exito el reporte ha sido enviado a mí Creadora. Tendrá una respuesta pronto. Si se detecta que el reporte es Falso será Ignorado el reporte.*`)
+  m.reply(`*[⚠️] تم بنجاح، تم إرسال التقرير إلى مطوري.  سيكون لديك الجواب قريبا.  إذا تم اكتشاف أن التقرير خاطئ، فسيتم تجاهل التقرير.*`)
 
 }
 
 handler.help = ['reporte', 'request'].map(v => v + ' <teks>')
 handler.tags = ['owner']
 handler.exp = 100 
-handler.command = /^(report|request|reporte|bugs|bug|report-owner|reportes|reportar)$/i 
+handler.command = /^(ابلاغ|request|reporte|bugs|bug|report-owner|reportes|reportar)$/i 
 export default handler
