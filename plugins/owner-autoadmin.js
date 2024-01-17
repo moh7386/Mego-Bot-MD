0d15b38da29fa9ca5fab57a9daf31eb306256de5
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-17 22:51:08 +0200

    Update owner-autoadmin.js

--- plugins/owner-autoadmin.js
+++ plugins/owner-autoadmin.js
@@ -1,7 +1,7 @@
 var handler = async(m, { conn, isAdmin }) => {
 
 if (m.fromMe) return
-if (isAdmin) return conn.reply(m.chat, '✅📌 *USTED YA ES ADMIN*', m, fake, )
+if (isAdmin) return conn.reply(m.chat, '✅📌 *انت بالفعل زعيم يا مطوري*', m, fake, )
 
 try {
 
@@ -9,12 +9,12 @@ await conn.groupParticipantsUpdate(m.chat, [m.sender], 'promote')
 
 } catch {
 
-await conn.reply(m.chat, '🏷️ *Ocurrió un fallo*', m, fake, )}
+await conn.reply(m.chat, '🏷️ *حدث فشل*', m, fake, )}
 
 }
 handler.help = ['autoadmin']
 handler.tags = ['owner']
-handler.command = /^autoadmin|tenerpoder$/i
+handler.command = /^autoadmin|ارفعني$/i
 
 handler.rowner = true
 handler.group = true
