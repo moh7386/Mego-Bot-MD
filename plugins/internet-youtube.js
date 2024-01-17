2f1c2d3255ac21b2b0c727002e44ca47586f54c7
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-17 20:13:34 +0200

    Update internet-youtube.js

--- plugins/internet-youtube.js
+++ plugins/internet-youtube.js
@@ -2,23 +2,23 @@ import yts from 'yt-search'
 
 var handler = async (m, { text, conn, args, command, usedPrefix }) => {
 
-if (!text) return conn.reply(m.chat, `*⚠️ ESCRIBA EL NOMBRE DE UN VIDEO O UN CANAL DE YOUTUBE*`, m)
+if (!text) return conn.reply(m.chat, `*⚠️ اكتب اسم الفيديو أو قناة اليوتيوب*`, m)
 let results = await yts(text)
 let tes = results.all
 let teks = results.all.map(v => {
 switch (v.type) {
-case 'video': return `📑 *TÍTULO:* ${v.title}
-🔗 *ENLACE:* ${v.url}
-⏰ *DURACIÓN:* ${v.timestamp}
-🔎 *SUBIDO:* ${v.ago}
-👀 *VISTAS:* ${v.views}`}}).filter(v => v).join('\n\n••••••••••••••••••••••••••••••••••••\n\n')
+case 'video': return `📑 *عنوان:* ${v.title}
+🔗 *اللينك:* ${v.url}
+⏰ *الوقت:* ${v.timestamp}
+🔎 *تم النشر:* ${v.ago}
+👀 *الاراء:* ${v.views}`}}).filter(v => v).join('\n\n••••••••••••••••••••••••••••••••••••\n\n')
 
 conn.sendFile(m.chat, tes[0].thumbnail, 'yts.jpeg', teks, fkontak, m)
 
 }
 handler.help = ['ytsearch']
 handler.tags = ['internet:']
-handler.command = /^playlist|ytbuscar|yts(earch)?$/i
+handler.command = /^playlist|يوتيوب|yts(earch)?$/i
 
 handler.exp = 70
 handler.limit = 1
