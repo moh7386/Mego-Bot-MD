1a006be1569c9ee349f8d113ca48962c47857cce
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-17 21:56:06 +0200

    Update grupo-listawarn.js

--- plugins/grupo-listawarn.js
+++ plugins/grupo-listawarn.js
@@ -3,9 +3,9 @@ const handler = async (m, {conn, isOwner}) => {
   const warns = global.db.data.users.warn;
   const user = global.db.data.users;
   const imagewarn = './src/warn.jpg';
-  const caption = `⚠️ المستخدمين المتحذرين\n 
+  const caption = `⚠️ تحذيرات المستخدمين\n 
 *╔═══════════════════·•*
-║ *المجموع : ${adv.length} المستخدمين* ${adv ? '\n' + adv.map(([jid, user], i) => `
+║ *العدد : ${adv.length} المستخدمين* ${adv ? '\n' + adv.map(([jid, user], i) => `
 ║
 ║ 1.- ${isOwner ? '@' + jid.split`@`[0] : jid} *(${user.warn}/3)*\n║\n║ - - - - - - - - -`.trim()).join('\n') : ''}
 *╚══════════════════·•*`;
