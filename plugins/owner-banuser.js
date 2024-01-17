e1b1e91289c9c69ade7d594aafc9db1e2da1d160
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-17 23:07:31 +0200

    Update owner-banuser.js

--- plugins/owner-banuser.js
+++ plugins/owner-banuser.js
@@ -1,13 +1,13 @@
 let handler = async (m, { conn, participants, usedPrefix, command }) => {
-let BANtext = `*⚠️ ETIQUETE A UNA PERSONA*\n\n💡 EJEMPLO\n*${usedPrefix + command} @${global.suittag}*`
+let BANtext = `*⚠️ ضع علامه ع الشخص*\n\n💡 مثال\n*${usedPrefix + command} @${global.suittag}*`
 if (!m.mentionedJid[0] && !m.quoted) return m.reply(BANtext, m.chat, { mentions: conn.parseMention(BANtext)})
 let who
 if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 else who = m.chat
 let users = global.db.data.users
 users[who].banned = true
-m.reply('*⏤͟͟͞͞🍓ESTE USUARIO FUE BANEADO CON ÉXITO*\n\n*[🛑] YA NO PUEDE USAR CURIOSITY BOT-MD*')    }
+m.reply('*⏤͟͟͞͞🚯تم حظر هذا المستخدم بنجاح*\n\n*[🔒] لم تعد بأمكانك استخدام ميجو بوت*')    }
 handler.tags = ['owner']
-handler.command = /^banuser$/i
+handler.command = /^بان$/i
 handler.rowner = true
 export default handler
