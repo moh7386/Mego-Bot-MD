77f6ed27d0c0401c6c765470328cb24e01867141
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-17 02:00:01 +0200

    Update grupo-setpp.js

--- plugins/grupo-setpp.js
+++ plugins/grupo-setpp.js
@@ -4,14 +4,14 @@ let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || q.mediaType || ''
 if (/image/.test(mime)) {
 let img = await q.download()
-if (!img) return conn.reply(m.chat, '🎌 *Debera etiquetar una imagen*', m, fake, )
-await conn.updateProfilePicture(m.chat, img).then(_ => conn.reply(m.chat, '✅ *Imagen actualizada con éxito*', m, fake, ))
-} else return conn.reply(m.chat, '🎌 *Debera etiquetar una imagen*', m, fake, )
+if (!img) return conn.reply(m.chat, '🎌 *سوف تحتاج إلى ريبلاي علامة على الصورة*', m, fake, )
+await conn.updateProfilePicture(m.chat, img).then(_ => conn.reply(m.chat, '✅ *تم تغيير الصوره بنجاح*', m, fake, ))
+} else return conn.reply(m.chat, '🎌 *سوف تحتاج إلى ريبلاي علامة على الصورة*', m, fake, )
 
 }
 handler.help = ['setpp', 'setppgc', 'setppgroup']
 handler.tags = ['grupo']
-handler.command = /^setpp(group|grup|gc)?$/i
+handler.command = /^setpp(group|grup|gc|تغييرالصورة)?$/i
 
 handler.group = true
 handler.admin = true
