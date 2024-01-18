01cfa2535eb8d3086ace484a8a629139eda11163
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-18 08:40:28 +0200

    Update propietario(a)-listprim_vip.js

--- plugins/propietario(a)-prumiom_vip.js
+++ plugins/propietario(a)-prumiom_vip.js
@@ -1,11 +1,12 @@
+
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 let prem = global.prems.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid) 
-let teks = `▢ *المستخدمين البريميوم*\n─────────────\n` + prem.map(v => '❈↲ @' + v.replace(/@.+/, '')).join`\n`
+let teks = `▢ *المستخدمين VIP*\n─────────────\n` + prem.map(v => '- @' + v.replace(/@.+/, '')).join`\n`
 m.reply(teks, null, {mentions: conn.parseMention(teks)})
 
 }
 handler.help = ['listprem']
 handler.tags = ['main']
-handler.command = ['listprem', 'المميزين', 'البريميوم'] 
+handler.command = ['المميزين', 'premlist', 'listpremium'] 
 
 export default handler
