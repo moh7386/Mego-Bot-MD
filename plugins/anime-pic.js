091f898309575eee28772460a7f2cf5ac095cd95
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-18 13:19:39 +0200

    Add files via upload

---
+++ plugins/anime-pic.js
@@ -0,0 +1,21 @@
+import fetch from 'node-fetch'
+let handler = async (m, {
+    conn,
+    usedPrefix
+}) => {
+    await conn.sendMessage(m.chat, {
+        react: {
+            text: '🔥',
+            key: m.key,
+        }
+    })
+    let res = await fetch('https://api.waifu.pics/sfw/waifu')
+    if (!res.ok) throw await res.text()
+    let json = await res.json()
+    if (!json.url) throw 'Error!'
+    await conn.sendFile(m.chat, json.url, null, "*『🔥┇𝙼𝙴𝙶𝙾-𝙱𝙾𝚃』*", m)
+}
+handler.help = ['waifu']
+handler.tags = ['internet']
+handler.command = /^(مراتي)$/i
+export default handler
