a2437fe8fff951fdc5e6212cc672ce2516ed3494
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-18 12:19:50 +0200

    Update rpg-unreg.js

--- plugins/rpg-unreg.js
+++ plugins/rpg-unreg.js
@@ -1,14 +1,14 @@
 import { createHash } from 'crypto'
 let handler = async function (m, { args }) {
-if (!args[0]) throw `*⚠️ INGRESE SU NÚMERO DR SÉRIE, SI NO SABE CUAL ES USE #myns*`
+if (!args[0]) throw `*⚠️ أدخل رقمك التسلسلي، إذا كنت لا تعرف ما هو استخدامه #ايدي*`
 let user = global.db.data.users[m.sender]
 let sn = createHash('md5').update(m.sender).digest('hex')
-if (args[0] !== sn) throw `*⚠️ VERIFIQUE QUE SU NÚMERO DE SÉRIE SEA CORRECTO*`
+if (args[0] !== sn) throw `*⚠️ تأكد من صحة الرقم التسلسلي الخاص بك*`
 user.registered = false
-m.reply(`*📇 USTED YA NO ESTÁ REGISTRADO*`)
+m.reply(`*📇 لم تعد مسجلا*`)
 }
-handler.help = ['', 'ister'].map(v => 'unreg' + v + ' <numero de serie>')
+handler.help = ['', 'ister'].map(v => 'unreg' + v + ' <رقم سري>')
 handler.tags = ['rg']
-handler.command = /^unreg(ister)?$/i
+handler.command = /^unreg(ister)|الغاء_التسجيل?$/i
 handler.register = true
 export default handler
