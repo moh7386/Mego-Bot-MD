797ed6960bc0c2f0696323e85d0cada766d1ce23
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-18 12:31:37 +0200

    Update transformador-togifaud.js

--- plugins/transformador-togifaud.js
+++ plugins/transformador-togifaud.js
@@ -1,17 +1,17 @@
 var handler = async (m, {conn, usedPrefix, command}) => {
 
-if (!m.quoted) throw `*⚠️ RESPONDA A UN VÍDEO QUE DESEE CONVERTIR A GIF CON AUDIO*` 
+if (!m.quoted) throw `*⚠️رد ع فيديو لتحويله جيفgif*` 
 const q = m.quoted || m
 const mime = (q.msg || q).mimetype || ''
-if (!/(mp4)/.test(mime)) throw `*⚠️ MÍMICA ${mime} NO SOPORTADA*` 
-m.reply('*🚀 C A R G A N D O*')
+if (!/(mp4)/.test(mime)) throw `*⚠️ تشابه ${mime} غير مدعوم*` 
+m.reply('*🚀 تم التحويل بوساطه ميجو*')
 const media = await q.download()
 conn.sendMessage(m.chat, {video: media, gifPlayback: true, caption: '*🚀🚀*'}, {quoted: m})
 
 }
 handler.help = ['togifaud']
 handler.tags = ['transformador']
-handler.command = ['togifaud']
+handler.command = ['جيف']
 
 handler.limit = true
  
