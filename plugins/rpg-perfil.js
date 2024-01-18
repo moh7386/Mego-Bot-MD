7dac6662dc58b021756ce3cdbba98ef00792070c
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-18 10:36:27 +0200

    Update rpg-perfil.js

--- plugins/rpg-perfil.js
+++ plugins/rpg-perfil.js
@@ -8,24 +8,24 @@ let handler = async (m, { conn }) => {
     let { premium, level, diamond, exp, lastclaim, registered, regTime, age } = global.db.data.users[m.sender]
     let username = conn.getName(who)
     let name = conn.getName(who)
-    let fkon = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
+    let fkon = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '201012531172@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
     let str = `
-. . . *🔰 P E R F I L 🔰* . . .
-💌 • *Nombre:* ${username} 
-📧 • *Tag:* @${who.replace(/@.+/, '')}
-📞 • *Numero:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
-🔗 • *Link:* https://wa.me/${who.split`@`[0]}
-🎨 • *Edad:* ${registered ? age : ''}
-💎 • *Limite: ${diamond} Usos* 
-📑 • *Registrado:* ${registered ? '✅': '❌'}
-🌟 • *Premium:* ${premium ? "✅\n⏰ • *PremiumTime:* ${clockString(user.premiumTime)}" :"❌"}
+. . . *🔰 بروفايلك 🔰* . . .
+💌 • *الاسم:* ${username} 
+📧 • *المنشن:* @${who.replace(/@.+/, '')}
+📞 • *الرقم:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
+🔗 • *اللينك:* https://wa.me/${who.split`@`[0]}
+🎨 • *العمر:* ${registered ? age : ''}
+💎 • *الماسك: ${diamond} Usos* 
+📑 • *مسجل:* ${registered ? '✅': '❌'}
+🌟 • *بريم:* ${premium ? "✅\n⏰ • *وقت المميز:* ${clockString(user.premiumTime)}" :"❌"}
 `.trim()
   //m.react(🌟) 
  conn.sendFile(m.chat, pp, 'perfil.jpg', str, fkon, false, { mentions: [who] })
 }
 handler.help = ['profile [@user]']
 handler.tags = ['rg']
-handler.command = /^perfil|pp$/i
+handler.command = /^بروفايل|انا$/i
 export default handler
 
 const more = String.fromCharCode(8206)
@@ -36,5 +36,5 @@ function clockString(ms) {
   let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [d, ' *Dias ☀️*\n ', h, ' *Horas 🕐*\n ', m, ' *Minutos ⏰*\n ', s, ' *Segundos ⏱️* '].map(v => v.toString().padStart(2, 0)).join('')
+  return [d, ' *Dias ☀️*\n ', h, ' *ساعات 🕐*\n ', m, ' *دقائق ⏰*\n ', s, ' *ثواني ⏱️* '].map(v => v.toString().padStart(2, 0)).join('')
 }
