b5188c01e4a7b671bf6a4b90f4a5702ba3f44849
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-18 02:00:08 +0200

    Update propietario(a)-restart.js

--- plugins/propietario(a)-restart.js
+++ plugins/propietario(a)-restart.js
@@ -2,14 +2,14 @@ import { spawn } from 'child_process'
 let handler = async (m, { conn, isROwner, text }) => {
     if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
     if (conn.user.jid == conn.user.jid) {
-    await m.reply('🔄 ＲＥＩＮＩＣＩＡＤＯ...')
+    await m.reply('🔄 جاري اعاده تشغيل البوت...')
     process.send('reset')
   } else throw 'eh'
 }
 
 handler.help = ['restart']
 handler.tags = ['owner']
-handler.command = ['restart','reiniciar'] 
+handler.command = ['ريستارت','reiniciar'] 
 
 handler.rowner = true
 
