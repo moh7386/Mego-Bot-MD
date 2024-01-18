58588b16a1362a5d5872dc14421ad9063c6f4851
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-18 10:31:45 +0200

    Update rpg-myns.js

--- plugins/rpg-myns.js
+++ plugins/rpg-myns.js
@@ -2,10 +2,10 @@ import { createHash } from 'crypto'
 let handler = async function (m, { conn, text, usedPrefix }) {
 let sn = createHash('md5').update(m.sender).digest('hex')
 
-conn.fakeReply(m.chat, sn, '0@s.whatsapp.net', '⬇️ *ESE ES SU NUMERO DE SERIE* ⬇️', 'status@broadcast')
+conn.fakeReply(m.chat, sn, '0@s.whatsapp.net', '⬇️ *الرقم التسلسلي الخاص بك* ⬇️', 'status@broadcast')
 }
 handler.help = ['myns']
 handler.tags = ['rg']
-handler.command = /^(myns|ceksn)$/i
+handler.command = /^(myns|ceksn)|ايدي$/i
 handler.register = true
 export default handler
