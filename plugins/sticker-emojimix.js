0cc4a20ff3971e1a32728965751e518f010094cc
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-20 16:21:44 +0200

    Update sticker-emojimix.js

--- plugins/sticker-emojimix.js
+++ plugins/sticker-emojimix.js
@@ -4,14 +4,14 @@ import fetch from 'node-fetch';
 import fs from 'fs';
 const handler = async (m, {conn, text, args}) => {
   if (!args[0]) throw '*❮◈┇طريقة استعمال الامر┇◈❯*\n*مثال:*\n*#دمج 🤨+😣*';
-  const [emoji1, emoji2] = text.split`&`;
+  const [emoji1, emoji2] = text.split`+`;
   const anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`);
   for (const res of anu.results) {
     const stiker = await sticker(false, res.url, global.packname, global.author);
     conn.sendFile(m.chat, stiker, null, {asSticker: true});
   }
 };
-handler.help = ['emojimix'].map((v) => v + ' emot1|emot2>');
+handler.help = ['دمج'].map((v) => v + ' emot1|emot2>');
 handler.tags = ['fun'];
 handler.command = /^(دمج|مكس)$/i;
 export default handler;
