0a5cf6d02f94dc529f05ff7fa1e9868249f2494d
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-21 23:04:45 +0200

    Update transformador-tomp3.js

--- plugins/transformador-tomp3.js
+++ plugins/transformador-tomp3.js
@@ -2,13 +2,13 @@ import {toAudio} from '../lib/converter.js';
 const handler = async (m, {conn, usedPrefix, command}) => {
   const q = m.quoted ? m.quoted : m;
   const mime = (q || q.msg).mimetype || q.mediaType || '';
-  if (!/لصوتت|لصوت/.test(mime)) throw `*[❗ملحوظه❗] رد ع فيديو يحب*`;
+  if (!/لصوت/.test(mime)) throw `*[❗ملحوظه❗] رد ع فيديو يحب*`;
   const media = await q.download();
   if (!media) throw '*[❗معلومه❗] حدث خطأ ما*';
   const audio = await toAudio(media, 'mp4');
   if (!audio.data) throw '*[❗معلومه❗]نأسف، حدث خطأ أثناء تحويل تسجيل الصوت إلى نص. يرجى المحاولة مرة أخرى*';
   conn.sendMessage(m.chat, {audio: audio.data, mimetype: 'audio/mpeg'}, {quoted: m});
 };
-handler.alias = ['tomp3', 'toaudio'];
-handler.command = /^to(mp3|audio)|لصوت$/i;
+handler.alias = ['لصوت'];
+handler.command = /^لصوت$/i;
 export default handler;
