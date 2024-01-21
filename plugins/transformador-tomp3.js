390fd035ebf12689f34baf84b4f2fe14408cc5e6
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-22 00:49:35 +0200

    Update transformador-tomp3.js

--- plugins/transformador-tomp3.js
+++ plugins/transformador-tomp3.js
@@ -4,7 +4,7 @@ var handler = async (m, { conn, usedPrefix, command }) => {
 
 let q = m.quoted ? m.quoted : m
 let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-if (!/video|audio/.test(mime)) throw `*⚠️ RESPONDA A UN VIDEO O NOTA DE VOS QUE DESEE CONVERTIR A AUDIO/MP3*`
+if (!/video|audio/.test(mime)) throw `*⚠️ قم بالرد علي فيديو لتحويله لصوت*`
 let media = await q.download?.()
 if (!media && !/video/.test(mime)) throw '*⚠️ OCURRIÓ UN ERROR, VUELVA A INTENTARLO*'
 if (!media && !/audio/.test(mime)) throw '*⚠️ OCURRIÓ UN ERROR, VUELVA A INTENTARLO*'
@@ -16,7 +16,7 @@ conn.sendFile(m.chat, audio.data, 'error.mp3', '', m, null, { mimetype: 'audio/m
 }
 handler.help = ['tomp3']
 handler.tags = ['transformador']
-handler.command = /^to(mp3|a(udio)?)$/i
+handler.command = ['لصوت']
 
 handler.limit = true
 
