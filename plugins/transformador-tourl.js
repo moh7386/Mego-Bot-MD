cf204b611fcf57aeb8dc655911b64e7ebe21444a
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-23 09:22:12 +0200

    Update transformador-tourl.js

--- plugins/transformador-tourl.js
+++ plugins/transformador-tourl.js
@@ -26,7 +26,7 @@ conn.reply(m.chat, info, m, { contextInfo: { externalAdReply :{ mediaUrl: ig, me
 }
 handler.help = ['tourl']
 handler.tags = ['transformador']
-handler.command = /^(تليجراف|لرابط)$/i
+handler.command = /^لرابط$/i
 
 handler.limit = true
 
