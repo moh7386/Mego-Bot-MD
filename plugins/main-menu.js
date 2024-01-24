8a26a4023ce9ee36db9834f58b2314d1776fedbd
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-24 20:08:43 +0200

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -19,7 +19,7 @@
        let _uptime = process.uptime() * 1000
        let uptime = clockString(_uptime)
    let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-   if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
+   if (!(who in global.db.data.users)) throw `✳️ لم يتم العثور على المستخدم في قاعدة البيانات الخاصة بي`
    let videoUrl = 'https://telegra.ph/file/e27416d79b6803287c628.mp4'
    let user = global.db.data.users[who]
    let { name, exp, diamond, lastclaim, registered, regTime, age, level, role, warn } = global.db.data.users[who]
