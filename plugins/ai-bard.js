504af8df9e4fdf799ca2d0b5c75f7b02b8717cbe
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-24 12:23:14 +0200

    Update ai-bard.js

--- plugins/ai-bard.js
+++ plugins/ai-bard.js
@@ -2,25 +2,23 @@ import fetch from 'node-fetch'
 
 var handler = async (m, { text,  usedPrefix, command }) => {
 
-if (!text) return conn.reply(m.chat, `🎌 *Ingrese una petición*\n\nEjemplo, !bard Conoces CuriosityBot-MD?`, m, fake, )
+if (!text) return conn.reply(m.chat, `🎌 *أدخل الطلب*\n\nمثال, !بارد تعرف كريستيانو`, m, fake, )
 
 try {
 
 conn.sendPresenceUpdate('composing', m.chat)
-var apii = await fetch(`https://aemt.me/bard?text=${text}`)
+var apii = await fetch(`https://api.yanzbotz.my.id/bard?text=${text}`)
 var res = await apii.json()
 await m.reply(res.result)
 
 } catch (error) {
 console.error(error)
-return conn.reply(m.chat, `*🚩 Ocurrió un fallo*`, m, fake, )
+return conn.reply(m.chat, `*🚩 خطأ*`, m, fake, )
 }
 
 }
-handler.command = ['bard']
-handler.help = ['bard']
+handler.command = ['بارد']
+handler.help = ['بارد']
 handler.tags = ['ai']
-
-handler.premium = false
 
 export default handler
