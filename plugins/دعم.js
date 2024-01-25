38a7ec1e340571e6aac96910ff46bc3ffb70c879
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-25 19:42:26 +0200

    Update دعم.js

--- plugins/دعم.js
+++ plugins/دعم.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, command, text }) => {
 let love = `
 *لدعم البوت تستطيع ان تفعل:-*
 *1_تستطيع دخول قروب دعم البوت لدعمه.↯↯*
-*https://chat.whatsapp.com/HiP4wq4KssO50q78Wacv0J*
+*https://chat.whatsapp.com/KF1ouG4qNb4J0dI0JTH8Ht*
 
 *2_وتستطيع ان تتابع البوت على الانستا:-*
 *https://instagram.com/mego51_51?igshid=NTc4MTIwNjQ2YQ==* 
