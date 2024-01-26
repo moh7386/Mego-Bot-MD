0f49fc1648db9eb67bca41de284a1b5a1b068564
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-26 12:50:31 +0200

    Update menu tools.js

--- plugins/menu tools.js
+++ plugins/menu tools.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
   let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
   let videoUrl = 'https://telegra.ph/file/af11f126f24c4e3ac3848.mp4'
   let { name } = global.db.data.users[who]
-  
+  m.react('⚙️')
 let str = `                  ✥━─━⌬ 𝒎𝒆𝒈𝒐_𝒃𝒐𝒕 ⌬━─━✥
 【..≼قــســم الادوات≽..】
                    ⋄━───═◞⬪⋇⬪◟═───━⋄
