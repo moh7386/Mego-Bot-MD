8601db49a9a7c69389839d93446c9f5ef02e962b
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-26 12:24:19 +0200

    Update menu qran.js

--- plugins/menu qran.js
+++ plugins/menu qran.js
@@ -1,9 +1,11 @@
-let handler = async (m, { conn, command, text }) => {
-let love = `
-*✥━─━⌬ 𝒎𝒆𝒈𝒐_𝒃𝒐𝒕 ⌬━─━✥*
-
-【..≼قــســم الــديــن≽..】
-*⋄━───═◞⬪⋇⬪◟═───━⋄*
+let handler = async (m, { conn }) => {
+  let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+  let videoUrl = 'https://telegra.ph/file/c1f3adc59818e62a90a8f.mp4'
+  let { name } = global.db.data.users[who]
+  
+let str = `                  ✥━─━⌬ 𝒎𝒆𝒈𝒐_𝒃𝒐𝒕 ⌬━─━✥
+【..≼قــســم الدين≽..】
+                   ⋄━───═◞⬪⋇⬪◟═───━⋄
 ❐..🕋╎❯ .ايه-الكرسي⌉
 ❐..🕋╎❯ .الله⌉
 ❐..🕋╎❯ .قران⌉
@@ -11,10 +13,17 @@ let love = `
 ❐..🕋╎❯ .سور⌉
 ❐..🕋╎❯ .حديث⌉
 ❐..🕋╎❯ .ذكر⌉
-*✥━─━⌬ 𝒎𝒆𝒈𝒐_𝒃𝒐𝒕 ⌬━─━✥*
- `.trim()
-m.reply(love, null, { mentions: conn.parseMention(love) })}
-handler.help = ['love']
-handler.tags = ['fun']
-handler.command = /^(الدين)$/i
+                    ✥━─━⌬ 𝒎𝒆𝒈𝒐_𝒃𝒐𝒕 ⌬━─━✥
+`
+  conn.sendMessage(m.chat, {
+           video: { url: videoUrl }, caption: str,
+     mentions: [m.sender,global.conn.user.jid],
+     gifPlayback: true,gifAttribution: 0
+       }, { quoted: m });
+   };
+
+handler.help = ['main']
+handler.tags = ['group']
+handler.command = ['الدين']
+
 export default handler
