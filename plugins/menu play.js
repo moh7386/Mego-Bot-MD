876a9265b46d793615afd69f49cb547fe769b06d
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-26 12:48:17 +0200

    Update menu play.js

--- plugins/menu play.js
+++ plugins/menu play.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
   let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
   let videoUrl = 'https://telegra.ph/file/c1f3adc59818e62a90a8f.mp4'
   let { name } = global.db.data.users[who]
-  
+  m.react('🎮')
 let str = `                  ✥━─━⌬ 𝒎𝒆𝒈𝒐_𝒃𝒐𝒕 ⌬━─━✥
 【..≼قــســم الترفيه≽..】
                    ⋄━───═◞⬪⋇⬪◟═───━⋄
