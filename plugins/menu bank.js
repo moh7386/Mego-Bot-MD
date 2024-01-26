58b94e4270337feb2b636dcb0f61655d2a47f980
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-26 12:08:54 +0200

    Update menu bank.js

--- plugins/menu bank.js
+++ plugins/menu bank.js
@@ -1,8 +1,12 @@
-let handler = async (m, { conn, command, text }) => {
-let love = `
-*✥━─━⌬ 𝒎𝒆𝒈𝒐_𝒃𝒐𝒕 ⌬━─━✥*
-【..≼قــســم الــبـنــك≽..】
-*⋄━───═◞⬪⋇⬪◟═───━⋄*
+let handler = async (m, { conn }) => {
+  let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+  let videoUrl = 'https://telegra.ph/file/65cfc6dd32215a624e9dc.mp4'
+  let { name } = global.db.data.users[who]
+  
+let str = `                  ✥━─━⌬ 𝒎𝒆𝒈𝒐_𝒃𝒐𝒕 ⌬━─━✥
+【..≼قــســم البنك≽..】
+                   ⋄━───═◞⬪⋇⬪◟═───━⋄
+
 ❐..💎╎❯ .البنك⌉
 ❐..💎╎❯ .راتب⌉
 ❐..💎╎❯ .شراء⌉
@@ -16,10 +20,17 @@ let love = `
 ❐..💎╎❯ .لفل⌉
 ❐..💎╎❯ .تسجيل⌉
 ❐..💎╎❯ .الغاء_التسجيل⌉
-*✥━─━⌬ 𝒎𝒆𝒈𝒐_𝒃𝒐𝒕 ⌬━─━✥*
- `.trim()
-m.reply(love, null, { mentions: conn.parseMention(love) })}
-handler.help = [ 'love' ]
-handler.tags = [ 'fun' ]
-handler.command = /^(البنك)$/i
+                    ✥━─━⌬ 𝒎𝒆𝒈𝒐_𝒃𝒐𝒕 ⌬━─━✥
+`
+  conn.sendMessage(m.chat, {
+           video: { url: videoUrl }, caption: str,
+     mentions: [m.sender,global.conn.user.jid],
+     gifPlayback: true,gifAttribution: 0
+       }, { quoted: m });
+   };
+
+handler.help = ['main']
+handler.tags = ['group']
+handler.command = ['البنك']
+
 export default handler
