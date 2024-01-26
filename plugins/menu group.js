a336c126ea38352e4581842a557eeb65ecb526a3
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-26 12:47:01 +0200

    Update menu group.js

--- plugins/menu group.js
+++ plugins/menu group.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
   let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
   let videoUrl = 'https://telegra.ph/file/73fd2daa9dca15ae5db80.mp4'
   let { name } = global.db.data.users[who]
-  
+  m.react('🔕')
 let str = `                  ✥━─━⌬ 𝒎𝒆𝒈𝒐_𝒃𝒐𝒕 ⌬━─━✥
 【..≼قــســم الجروبات≽..】
                    ⋄━───═◞⬪⋇⬪◟═───━⋄
