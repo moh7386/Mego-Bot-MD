8e0a95d351ae4e02370685c499760504f58bd702
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-26 12:22:28 +0200

    Update menu play.js

--- plugins/menu play.js
+++ plugins/menu play.js
@@ -1,10 +1,11 @@
-let handler = async (m, { conn, command, text }) => {
-
-let love = `
-
-*✥━─━⌬ 𝒎𝒆𝒈𝒐_𝒃𝒐𝒕 ⌬━─━✥*
-【..≼قــســم الــترفيــه≽..】
-*⋄━───═◞⬪⋇⬪◟═───━⋄*
+let handler = async (m, { conn }) => {
+  let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+  let videoUrl = 'https://telegra.ph/file/c1f3adc59818e62a90a8f.mp4'
+  let { name } = global.db.data.users[who]
+  
+let str = `                  ✥━─━⌬ 𝒎𝒆𝒈𝒐_𝒃𝒐𝒕 ⌬━─━✥
+【..≼قــســم الترفيه≽..】
+                   ⋄━───═◞⬪⋇⬪◟═───━⋄
 ❐..🎮╎❯ .اكس_او⌉
 ❐..🎮╎❯ .احذف_اللعبه⌉
 ❐..🎮╎❯ .تحدي⌉
@@ -46,16 +47,17 @@ let love = `
 ❐..🎮╎❯ .دين⌉
 ❐..🎮╎❯ .كوره⌉
 ❐..🎮╎❯ .رياضيات⌉
-*✥━─━⌬ 𝒎𝒆𝒈𝒐_𝒃𝒐𝒕 ⌬━─━✥*
+                    ✥━─━⌬ 𝒎𝒆𝒈𝒐_𝒃𝒐𝒕 ⌬━─━✥
+`
+  conn.sendMessage(m.chat, {
+           video: { url: videoUrl }, caption: str,
+     mentions: [m.sender,global.conn.user.jid],
+     gifPlayback: true,gifAttribution: 0
+       }, { quoted: m });
+   };
 
- `.trim()
-
-m.reply(love, null, { mentions: conn.parseMention(love) })}
-
-handler.help = ['love']
-
-handler.tags = ['fun']
-
-handler.command = /^(الترفيه)$/i
+handler.help = ['main']
+handler.tags = ['group']
+handler.command = ['الترفيه']
 
 export default handler
