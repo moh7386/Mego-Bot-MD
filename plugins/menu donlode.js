a65fd8a8e895e197a2e6549e04d29387148f4b5c
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-26 12:14:25 +0200

    Update menu donlode.js

--- plugins/menu donlode.js
+++ plugins/menu donlode.js
@@ -1,10 +1,12 @@
-let handler = async (m, { conn, command, text }) => {
-let love = `
-*✥━─━⌬ 𝒎𝒆𝒈𝒐_𝒃𝒐𝒕 ⌬━─━✥*
+let handler = async (m, { conn }) => {
+  let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+  let videoUrl = 'https://telegra.ph/file/c1f3adc59818e62a90a8f.mp4'
+  let { name } = global.db.data.users[who]
+  
+let str = `                  ✥━─━⌬ 𝒎𝒆𝒈𝒐_𝒃𝒐𝒕 ⌬━─━✥
+【..≼قــســم التحميلات≽..】
+                   ⋄━───═◞⬪⋇⬪◟═───━⋄
 
-【..≼قــســم التــحميــلات≽..】
-*⋄━───═◞⬪⋇⬪◟═───━⋄*
-❐..📥╎❯ .فيديو⌉
 ❐..📥╎❯ .فيديو2⌉
 ❐..📥╎❯ .اغنيه⌉
 ❐..📥╎❯ .اغاني⌉
@@ -30,10 +32,17 @@ let love = `
 ❐..📥╎❯ .جيف⌉
 ❐..📥╎❯ .بنت⌉
 ❐..📥╎❯ .جوجل⌉
-*✥━─━⌬ 𝒎𝒆𝒈𝒐_𝒃𝒐𝒕 ⌬━─━✥*
- `.trim()
-m.reply(love, null, { mentions: conn.parseMention(love) })}
-handler.help = ['love']
-handler.tags = ['fun']
-handler.command = /^(التحميلات)$/i
+                    ✥━─━⌬ 𝒎𝒆𝒈𝒐_𝒃𝒐𝒕 ⌬━─━✥
+`
+  conn.sendMessage(m.chat, {
+           video: { url: videoUrl }, caption: str,
+     mentions: [m.sender,global.conn.user.jid],
+     gifPlayback: true,gifAttribution: 0
+       }, { quoted: m });
+   };
+
+handler.help = ['main']
+handler.tags = ['group']
+handler.command = ['البنك']
+
 export default handler
