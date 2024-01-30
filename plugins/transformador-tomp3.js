17571433d96320e2ef685b4db1dd5b423c17c33c
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-30 14:13:34 +0200

    Update transformador-tomp3.js

--- plugins/transformador-tomp3.js
+++ plugins/transformador-tomp3.js
@@ -1,23 +1,17 @@
 import { toAudio } from '../lib/converter.js'
 
-var handler = async (m, { conn, usedPrefix, command }) => {
-
-let q = m.quoted ? m.quoted : m
-let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-if (!/video|audio/.test(mime)) throw `*⚠️ قم بالرد علي فيديو لتحويله لصوت*`
-let media = await q.download?.()
-if (!media && !/video/.test(mime)) throw '*⚠️ OCURRIÓ UN ERROR, VUELVA A INTENTARLO*'
-if (!media && !/audio/.test(mime)) throw '*⚠️ OCURRIÓ UN ERROR, VUELVA A INTENTARLO*'
-let audio = await toAudio(media, 'mp4')
-if (!audio.data && !/audio/.test(mime)) throw '*⚠️ OCURRIÓ UN ERROR, VUELVA A INTENTARLO*'
-if (!audio.data && !/video/.test(mime)) throw '*⚠️ OCURRIÓ UN ERROR, VUELVA A INTENTARLO*'
-conn.sendFile(m.chat, audio.data, 'error.mp3', '', m, null, { mimetype: 'audio/mp4' })
-
+let handler = async (m, { conn, usedPrefix, command }) => {
+    let q = m.quoted ? m.quoted : m
+   /* let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
+    if (!/video|audio/.test(mime)) throw `✳️ قم بالرد على الفيديو أو الملاحظة الصوتية التي تريد تحويلها إلى mp3 باستخدام الأمر :\n\n*${usedPrefix + command}*`*/
+    let media = await q.download?.()
+    if (!media) throw '❎ فشل تنزيل الوسائط'
+    let audio = await toAudio(media, 'mp4')
+    if (!audio.data) throw '❎ Error converting'
+    conn.sendFile(m.chat, audio.data, 'audio.mp3', '', m, null, { mimetype: 'audio/mp4' })
 }
 handler.help = ['tomp3']
-handler.tags = ['transformador']
-handler.command = ['لصوت']
-
-handler.limit = true
+handler.tags = ['fun']
+handler.command = /^to(mp3|a(udio)?)|لصوت$/i
 
 export default handler
