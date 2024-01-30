0cdc0cafa038ffb95872fa77c839446b9083bdfc
Botmego <150967600+Botmego@users.noreply.github.com>
2024-01-30 18:14:38 +0200

    Update audio-toptt.js

--- plugins/audio-toptt.js
+++ plugins/audio-toptt.js
@@ -3,7 +3,7 @@ import { toPTT } from '../lib/converter.js'
 let handler = async (m, { conn, usedPrefix, command }) => {
     let q = m.quoted ? m.quoted : m
     let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-    if (!/video|audio/.test(mime)) throw `✳️ Reply to the audio you want to convert to voice memo with :\n *${usedPrefix + command}*`
+    if (!/video|audio/.test(mime)) throw `✳️ قم بالرد على الصوت الذي تريد تحويله إلى مذكرة صوتية باستخدامه :\n *${usedPrefix + command}*`
     let media = await q.download?.()
     if (!media) throw '❎ Failed to download media'
     let audio = await toPTT(media, 'mp4')
@@ -13,6 +13,6 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 handler.help = ['toav']
 handler.tags = ['fun']
 
-handler.command = ['toav', 'tovn'] 
+handler.command = ['toav', 'لريك'] 
 
 export default handler
