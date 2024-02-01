9bf45ffd5347cd8cbe18a957ece20c8fc62105f0
Botmego <150967600+Botmego@users.noreply.github.com>
2024-02-01 12:56:54 +0200

    Create موت.js

---
+++ plugins/موت.js
@@ -0,0 +1,18 @@
+let handler = async (m, { conn }) => {
+  let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+  let name = conn.getName(who)
+  let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './Menu.png')
+  conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/overlay/wasted', {
+    avatar: pp, 
+  }), 'waste.png', `*『🔥┇𝙼𝙴𝙶𝙾-𝙱𝙾𝚃』*  
+╭━━━[ *⚰️* ]━━━━⬣
+┃ *☜لقد مات*
+╯──────────────────╰
+┃*☜ المرحوم*  *⌝ ${name}⌞*
+ ${KMA} `, m)
+}
+handler.limit = 4;
+handler.help = ['waste @user']
+handler.tags = ['fun']
+handler.command = ['موت'] 
+export default handler
