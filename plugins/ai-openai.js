7b1f98d519964e2763902d152eb6aa1ce58f4553
Botmego <150967600+Botmego@users.noreply.github.com>
2024-02-02 10:05:34 +0200

    Update ai-openai.js

--- plugins/ai-openai.js
+++ plugins/ai-openai.js
@@ -6,6 +6,7 @@ import { Configuration, OpenAIApi } from 'openai'
 const configuration = new Configuration({ organization: global.openai_org_id, apiKey: global.openai_key })
 const openaiii = new OpenAIApi(configuration)
 
+m.react('🗣️')
 var handler = async (m, { conn, text, usedPrefix, command }) => {
 
 if (usedPrefix == 'a' || usedPrefix == 'A') return
