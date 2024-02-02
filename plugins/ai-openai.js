a1b8fce3b9c0e57f56fe050567a52bb3b5594201
Botmego <150967600+Botmego@users.noreply.github.com>
2024-02-02 14:38:29 +0200

    Update ai-openai.js

--- plugins/ai-openai.js
+++ plugins/ai-openai.js
@@ -2,45 +2,27 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 import translate from '@vitalets/google-translate-api'
 import { Configuration, OpenAIApi } from 'openai'
-
-const configuration = new Configuration({ organization: global.openai_org_id, apiKey: global.openai_key })
-const openaiii = new OpenAIApi(configuration)
-
-var handler = async (m, { conn, text, usedPrefix, command }) => {
-
-if (usedPrefix == 'a' || usedPrefix == 'A') return
-if (!text) return conn.reply(m.chat, `*🎌 ادخل الطلب للرد عليك*\n\nمثال: !ميجو هات معلومعات عن الانمي`, m, fake)
-
+const configuration = new Configuration({ organization: global.openai_org_id, apiKey: global.openai_key });
+const openaiii = new OpenAIApi(configuration);
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+if (usedPrefix == 'a' || usedPrefix == 'A') return    
+if (!text) throw `*${lenguajeGB['smsAvisoMG']()}𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙐𝙉𝘼 𝙋𝙀𝙏𝙄𝘾𝙄𝙊𝙉 𝙊 𝙐𝙉𝘼 𝙊𝙍𝘿𝙀𝙉 𝙋𝘼𝙍𝘼 𝙐𝙎𝘼𝙍 𝙇𝘼 𝙁𝙐𝙉𝘾𝙄𝙊𝙉 𝘿𝙀𝙇 𝘾𝙃𝘼𝙏𝙂𝙋𝙏\n\n❏ 𝙀𝙅𝙀𝙈𝙋𝙇𝙊 𝘿𝙀 𝙋𝙀𝙏𝙄𝘾𝙄𝙊𝙉𝙀𝙎 𝙔 𝙊𝙍𝘿𝙀𝙉𝙀𝙎\n❏ ${usedPrefix + command} Recomienda un top 10 de películas de acción\n❏ ${usedPrefix + command} Codigo en JS para un juego de cartas`     
 try {
-conn.sendPresenceUpdate('composing', m.chat)
-
-// Traducir de indonesio a Arabic
-const translation = await translate(text, { from: 'en', to: 'ar' })
-const indonesianText = translation.text
-let syms = `مطوري هو ميججؤؤ`
-let res = await openaiii.ChatGpt(indonesianText, syms)
-
+conn.sendPresenceUpdate('composing', m.chat)  
+let syms = `Actuaras como un Bot de WhatsApp el cual fue creado por GataNina-Li, tu seras GataBot-MD`
+let res = await gpt.ChatGpt(text, syms)
 await m.reply(res.text)
-
-} catch (err) {
-try {
-let ia2 = await fetch(`https://aemt.me/openai?text=${text}`)
+} catch {
+try {   
+let ia2 = await fetch(`https://api.amosayomide05.cf/gpt/?question=${text}&string_id=${m.sender}`) //fetch(`https://api.ibeng.tech/api/info/openai?text=${text}&apikey=tamvan`)
 let resu2 = await ia2.json()
-m.reply(resu2.result.trim())
-} catch (err) {
-try {
-let tioress = await fetch(`https://api.lolhuman.xyz/api/openai-turbo?apikey=GataDios&text=${text}`)
+m.reply(resu2.response.trim())    
+} catch {        
+try {    
+let tioress = await fetch(`https://api.lolhuman.xyz/api/openai-turbo?apikey=${lolkeysapi}&text=${text}`)
 let hasill = await tioress.json()
-conn.reply(m.chat, `${hasill.result}`, m, fake, )
-} catch (err) {
-console.error(err)
-conn.reply(m.chat, '🚩 *خطأ*', m, fake, )
-}
-}}
-
-}
-handler.help = ['ia']
-handler.tags = ['ai']
-handler.command = ['ميجو', 'chatgpt', 'ia', 'بوت']
-
+m.reply(`${hasill.result}`.trim())   
+} catch {    
+}}}}
+handler.command = ['openai', 'chatgpt', 'ia', 'robot']
 export default handler
