0deb8ea82d40d52b702f3b5c99d1d0b3de64d2a2
Botmego <150967600+Botmego@users.noreply.github.com>
2024-02-02 09:54:13 +0200

    Update ai-openai.js

--- plugins/ai-openai.js
+++ plugins/ai-openai.js
@@ -9,7 +9,7 @@ const openaiii = new OpenAIApi(configuration)
 var handler = async (m, { conn, text, usedPrefix, command }) => {
 
 if (usedPrefix == 'a' || usedPrefix == 'A') return
-if (!text) return conn.reply(m.chat, `*🎌 ادخل الطلب للرد عليك*\n\nمثال: !ميجو هات معلومعات عن الانمي\n\nميجو  اكتب كود Js`, m, fake)
+if (!text) return conn.reply(m.chat, `*🎌 ادخل الطلب للرد عليك*\n\nمثال: !ميجو هات معلومعات عن الانمي`, m, fake)
 
 try {
 conn.sendPresenceUpdate('composing', m.chat)
